28185eda38da2216bef77428b3d6cda5ec48be40
Mahesh Murag <maheshmmurag@gmail.com>
2015-09-13 12:24:50 -0700

    started scales

---
+++ src/scales.java
@@ -0,0 +1,42 @@
+/*
+TASK: scales
+LANG: JAVA
+ID: maheshm2
+ */
+import java.io.*;
+import java.util.Arrays;
+
+public class scales {
+    static StreamTokenizer input;
+    public static void main(String[] args) throws java.io.IOException {
+        String prob = "scales";
+        input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
+        PrintWriter output=new PrintWriter(new FileWriter(prob+".out"));
+        int n = nextInt();
+        int c = nextInt();
+        int[] w = new int[n];
+        for (int i = 0; i < n; i++)
+            w[i]=nextInt();
+        long val = 0;
+        Arrays.sort(w);
+        for (int i = n-1; i >= 1; i--) {
+            if (w[i]+w[i-1] < c){
+                val += w[i] + w[i-1];
+                i--;
+            }
+//            else if (w[i] > c){
+//                i--;
+//            }
+//        if (w[i]+w[i-1]>c>w[i]), ??
+        }
+        System.out.println(val);
+        output.println();
+        output.close();
+
+    }
+    static int nextInt() throws IOException {
+        input.nextToken();
+        return (int) input.nval;
+    }
+}
+
