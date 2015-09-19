cdbdffe2e513ec233b6bfb08d8f4ffa919a904ee
Mahesh Murag <maheshmmurag@gmail.com>
2015-09-19 13:32:53 -0700

    legit catchcow

--- src/catchcow.java
+++ src/catchcow.java
@@ -3,44 +3,59 @@ TASK: catchcow
 LANG: JAVA
 ID: maheshm2
  */
+
 import java.io.*;
+import java.util.LinkedList;
 
 public class catchcow {
     static StreamTokenizer input;
-    static int n,k, min = Integer.MAX_VALUE;
-//    static int[] dp = new int[100001];
+    static int n, k;
     public static void main(String[] args) throws java.io.IOException {
         String prob = "catchcow";
-        input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
-        PrintWriter output=new PrintWriter(new FileWriter(prob+".out"));
-
+        input = new StreamTokenizer(new BufferedReader(new FileReader(prob + ".in")));
+        PrintWriter output = new PrintWriter(new FileWriter(prob + ".out"));
         n = nextInt();
-        k=nextInt();
-//        Arrays.fill(dp,-1);
-
-        int ans = Math.min(rec(n+1,n,1),rec(n-1,n,1));
-        ans = Math.min(ans, rec(n*2,n,1));
-
-        output.println(min);
+        k = nextInt();
+        int ans = solve();
+        System.out.println(ans);
         output.close();
-
     }
-
-    static int rec(int pt, int prev, int count){
-        if(pt>100000 || pt<0) return 0;
-//        System.out.println(pt+":"+prev+":"+count);
-        if(pt == k){
-            if (count<min) min = count;
-            return count;
+    public static int solve(){
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.push(new Integer(n));
+        boolean visited[] = new boolean[100001];
+        int previous[] = new int[100001];
+        LinkedList<Integer> graph = new LinkedList<Integer>();
+        for (int i = 0; i <= 100000; i++)
+            graph.add(new Integer(i));
+        int cou = 0;
+        while (!queue.isEmpty()) {
+            Integer top = queue.getFirst();
+            queue.removeFirst();
+            if (top == k) {
+                int x = k;
+                while (x != n) {
+                    x = previous[x];
+                    cou++;
+                }
+                break;
+            }
+            for (int x = 0; x < 3; x++) {
+                int val = 0;
+                switch(x){
+                    case 0: val = -1; break;
+                    case 1: val = 1; break;
+                    case 2: val = top; break;
+                }
+                if (top+val >= 0 && top+val <= 100000 && !visited[top + val]) {
+                    queue.add(graph.get(top + val));
+                    visited[top + val] = true;
+                    previous[top + val] = top;
+                }
+            }
         }
-//        if(dp[pt]!=-1) return dp[pt];
-        int out = Math.min((prev==pt+1||pt > k)?Integer.MAX_VALUE:rec(pt+1,pt, count+1),
-                (prev==pt-1 || pt < k)?Integer.MAX_VALUE:rec(pt-1,pt, count+1));
-        out = Math.min(out, (pt > k || pt == 0)?Integer.MAX_VALUE:rec(pt*2,pt, count+1));
-//        dp[pt] = out;
-        return out;
+        return cou;
     }
-
     static int nextInt() throws IOException {
         input.nextToken();
         return (int) input.nval;
