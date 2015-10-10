8bdb6f885beb25148b475e5970b28c730272d53d
Mahesh Murag <maheshmmurag@gmail.com>
2015-10-10 16:34:24 -0700

    ttime: cleaned up

--- src/ttime.java
+++ src/ttime.java
@@ -4,31 +4,28 @@ LANG: JAVA
 ID: maheshm2
  */
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 
 public class ttime {
     static StreamTokenizer input;
     static int n, m, q, id;
     static boolean[][] matrix;
     static int[] visited;
-
     public static void main(String[] args) throws IOException {
         String prob = "ttime";
+//        input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
         input = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-//        PrintWriter output = new PrintWriter(new FileWriter(prob + ".out"));
-        n = nextInt();
-        m = nextInt();
-        q = nextInt();
+        n = nextInt();m = nextInt();q = nextInt();
         matrix = new boolean[n + 1][n + 1];
         visited = new int[n + 1];
-        Arrays.fill(visited, 0);
         for (int i = 0; i < m; i++) {
             int one = nextInt(), two = nextInt();
             matrix[one][two] = true;
             matrix[two][one] = true;
         }
-//        System.out.println(nextInt());
         id = 1;
         for (int i = 1; i <= n; i++) {
             if(visited[i]==0){//not visited
@@ -37,23 +34,17 @@ public class ttime {
                 id++;
             }
         }
-
-        for (int i = 0; i < q; i++) {
-            int a = nextInt(); int b = nextInt();
-            System.out.println(visited[a]==visited[b]?"Y":"N");
-        }
-
+        for (int i = 0; i < q; i++)
+            System.out.println(visited[nextInt()]==visited[nextInt()]?"Y":"N");
     }
 
     static void dfs(int index){
         if(index<0 || index>=n+1)
             return;
         visited[index]=id;
-        for (int i = 1; i <= n; i++) {
-            if(matrix[i][index] && visited[i] == 0){
+        for (int i = 1; i <= n; i++)
+            if(matrix[i][index] && visited[i] == 0)
                 dfs(i);
-            }
-        }
     }
 
     static int nextInt() throws IOException {
